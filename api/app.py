857348021ade76e22b1a7af42170def2a778ab6a
Joshua <Jdwalli2003@yahoo.com>
2022-09-15 01:33:32 -0400

    Started flask server

--- api/app.py
+++ api/app.py
@@ -1,3 +1,9 @@
 from flask import Flask, Response, request, jsonify
 import os
 from zipfile import ZipFile
+
+""" Global Variables """
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
