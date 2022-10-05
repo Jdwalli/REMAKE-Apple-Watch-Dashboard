ff6b67d010c52898911ffd0192264b71e55e0a87
Joshua <Jdwalli2003@yahoo.com>
2022-10-05 14:26:13 -0400

    Created base routes

--- api/app.py
+++ api/app.py
@@ -5,5 +5,14 @@ from zipfile import ZipFile
 """ Global Variables """
 app = Flask(__name__)
 
+@app.route("/api/upload", methods=["POST"])
+def upload_export():
+   return NotImplementedError
+
+@app.route("/api/home", methods=["GET"])
+def home():
+    return NotImplementedError
+
+
 if __name__ == "__main__":
     app.run(debug=True)
