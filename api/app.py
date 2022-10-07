7831428a7187ae9c804ed99c4da52e85a54f1280
Joshua <Jdwalli2003@yahoo.com>
2022-10-07 00:40:04 -0400

    Created potential API routes

--- api/app.py
+++ api/app.py
@@ -9,10 +9,25 @@ app = Flask(__name__)
 def upload_export():
    return NotImplementedError
 
-@app.route("/api/home", methods=["GET"])
-def home():
+@app.route("/api/records", methods=["GET"])
+def send_records():
     return NotImplementedError
 
+@app.route("/api/workoutRecords", methods=["GET"])
+def send_workout_records():
+    return NotImplementedError
+
+@app.route("/api/summaryRecords", methods=["GET"])
+def send_summary_records():
+    return NotImplementedError
+
+@app.route("/api/ecgFiles", methods=["GET"])
+def send_ecg_file_data():
+    return NotImplementedError
+
+@app.route("/api/workoutFiles", methods=["GET"])
+def send_workout_file_data():
+    return NotImplementedError
 
 if __name__ == "__main__":
     app.run(debug=True)
