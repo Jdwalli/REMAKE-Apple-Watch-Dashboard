25d36ad9e84d430f9cb8d56f91f9b89792595eab
Joshua <Jdwalli2003@yahoo.com>
2022-10-16 17:10:20 -0400

    Added modal to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Sidebar } from "./components/navigation/Sidebar";
 import { Header } from "./components/navigation/Header";
 
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import ExportUpload from "./components/common/ExportUpload";
 
 function App() {
+  const showUploadModal = useSelector(
+    (state: RootState) => state.uploadModal.isOpen
+  );
+
   return (
     <Router>
       <div className="h-screen w-screen bg-gray-800">
         <Header />
         <Sidebar />
-        <div className="h-screen bg-primary ml-24"></div>
+        {showUploadModal ? (
+          <div className="flex h-screen">
+            <div className="m-auto">
+              <ExportUpload open={showUploadModal} />
+            </div>
+          </div>
+        ) : <div className="h-screen bg-primary ml-24"></div>}
+        
       </div>
     </Router>
   );
