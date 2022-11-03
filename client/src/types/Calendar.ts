fbdf037028e8fd7212fe74ae412eb09e72141c5a
Joshua <Jdwalli2003@yahoo.com>
2022-11-03 00:31:03 -0400

    Updated types

---
+++ client/src/types/Calendar.ts
@@ -0,0 +1,11 @@
+export interface WorkoutEvents {
+    workoutActivityType: string,
+    startDate: string, 
+}
+
+export interface CalendarProps {
+    workoutEvents: WorkoutEvents[]
+}
+
+
+
---
+++ client/src/types/Workout.ts
@@ -0,0 +1,10 @@
+export interface WorkoutsStatisticsProps {
+    WorkoutType: string,
+    MaxDuration: number,
+    MaxTotalDistance: number,
+    MaxTotalEnergyBurned: number,
+    MinDuration: number,
+    MinTotalDistance: number, 
+    MinTotalEnergyBurned: number
+}
+
