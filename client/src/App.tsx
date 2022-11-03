294f998d8a499eb80636f360480a9b8f00026693
Joshua <Jdwalli2003@yahoo.com>
2022-11-03 00:23:43 -0400

    Added workout page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,9 @@ import { useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import ExportUpload from "./components/common/ExportUpload";
 
+// Routes
+import WorkoutPage from "./pages/WorkoutPage";
+
 function App() {
   const showUploadModal = useSelector(
     (state: RootState) => state.uploadModal.isOpen
@@ -23,8 +26,13 @@ function App() {
               <ExportUpload open={showUploadModal} />
             </div>
           </div>
-        ) : <div className="h-screen bg-primary ml-24"></div>}
-        
+        ) : (
+          <div className="h-screen bg-primary ml-24">
+            <Routes>
+              <Route path="/workouts" element={<WorkoutPage />} />
+            </Routes>
+          </div>
+        )}
       </div>
     </Router>
   );
