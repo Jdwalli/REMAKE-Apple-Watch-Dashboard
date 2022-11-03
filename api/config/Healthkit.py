30a00d2dbf749986e4c65b8dae6d45de86439179
Joshua <Jdwalli2003@yahoo.com>
2022-11-03 00:18:59 -0400

    Created healthkit variables

---
+++ api/config/Healthkit.py
@@ -0,0 +1,3 @@
+Swimming_Location_Dict = {0: "Unknown", 1: "Pool", 2: "Open Water"}
+Stroke_Style_DIct = {0: "Unknown", 1: "Mixed", 2: "Freestyle",
+                     3: "Backstroke", 4: "Breaststroke", 5: "Butterfly"}
