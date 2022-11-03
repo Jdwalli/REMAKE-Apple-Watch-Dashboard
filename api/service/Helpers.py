b2b9cfbfb44117eb641d460fa1c0b26008820cfb
Joshua <Jdwalli2003@yahoo.com>
2022-11-03 00:32:49 -0400

    Created workout api functions

--- api/service/Helpers.py
+++ api/service/Helpers.py
@@ -37,8 +37,35 @@ def read_healthkit_data(dataType: str, dataName: str):
         return jsonify(pd.read_csv(var_path, low_memory=True).to_json(orient='records')), 200
     return jsonify({'Error' : f'File associated with {dataName} not found'}), 500
 
+def read_workout_events():
+    var_path = os.path.join(os.getcwd(), "data", 'Workouts', 'Workout.csv')
+    if os.path.exists(var_path):
+        df = pd.read_csv(var_path, low_memory=True, usecols=['workoutActivityType', 'startDate'])
+        df['startDate'] = pd.to_datetime(df['startDate']).dt.date.astype(str)
+        return jsonify(df.to_json(orient='records')), 200
+    return jsonify({'Error' : f'File associated with Workouts not found'}), 500
+
 def read_workout_route_data(route: str):
     var_path = os.path.join(os.getcwd(), "data", 'Workouts', 'workout-routes', f'{route}.csv')
     if os.path.exists(var_path):
         return jsonify(pd.read_csv(var_path, low_memory=True).to_json(orient='records')), 200
     return jsonify({'Error' : 'Route {route} not found'}), 500
+
+
+def read_workout_statistics():
+    var_path = os.path.join(os.getcwd(), "data", 'Workouts', 'Workout.csv')
+    if os.path.exists(var_path):
+        df = pd.read_csv(var_path, low_memory=True, usecols=['workoutActivityType', 'duration', 'unit',  'totalDistance', 'totalDistanceUnit', 'totalEnergyBurned', 'totalEnergyBurnedUnit', 'startDate'])
+        max_result = df.groupby('workoutActivityType')[['duration', 'totalDistance', 'totalEnergyBurned']].agg('max').reset_index()
+        max_result = max_result.rename(columns={'workoutActivityType': 'WorkoutType', 'duration': 'MaxDuration', 'totalDistance' : 'MaxTotalDistance', 'totalEnergyBurned' : 'MaxTotalEnergyBurned'})
+        min_result = df.groupby('workoutActivityType')[['duration', 'totalDistance', 'totalEnergyBurned']].agg('min').reset_index()
+        min_result = min_result.rename(columns={'workoutActivityType': 'WorkoutType', 'duration': 'MinDuration', 'totalDistance' : 'MinTotalDistance', 'totalEnergyBurned' : 'MinTotalEnergyBurned'})
+        joined_results = pd.merge(max_result, min_result)
+        return jsonify(joined_results.to_json(orient='records')), 200
+    return jsonify({'Error' : f'File associated with Workouts not found'}), 500
+
+
+
+
+
+
