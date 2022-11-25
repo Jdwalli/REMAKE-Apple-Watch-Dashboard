7cb6a5b8c63fba5f0b7af13615b2b6a3c2802fc7
Joshua <Jdwalli2003@yahoo.com>
2022-11-25 15:49:08 -0500

    Created icon map component

---
+++ client/src/components/common/iconMap.tsx
@@ -0,0 +1,25 @@
+import {
+    FaWalking,
+    FaFire,
+    FaRunning,
+    FaMale,
+    FaHeadphones,
+  } from "react-icons/fa";
+
+import {
+    TbStairs
+} from 'react-icons/tb'
+  
+
+export const ActivityIconMap = {
+    'Step Count' : <FaWalking />,
+    'Active Energy Burned' : <FaFire />,
+    'Apple Exercise Time' : <FaRunning />,
+    'Apple Stand Time' : <FaMale />,
+    'Flights Climbed' : <TbStairs />,
+    'Audio Exposure' : <FaHeadphones />
+}
+
+
+  
+  
